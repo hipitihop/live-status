cb8ae3ae0cdae7fda1b82bf3957fe81a9dade80b
hipitihop <anonymous.amphibian@gmail.com>
2021-09-12 12:41:56 +1000

    Cleanup main() to do mqtt connect and wait and use logging instead of print.
    - use logging instead of print
    - add optional transmit-posture.service definition
    - add transmit-posture.sh

--- transmit-posture.py
+++ transmit-posture.py
@@ -1,3 +1,4 @@
+import logging
 import json
 import platform
 import re
@@ -7,7 +8,7 @@ from datetime import datetime
 from paho.mqtt import client as mqtt_client
 
 """
-Periodically checks the status of video device and microphones.
+Periodically checks the status of both video device and microphones.
 Whenever the combined status changes, post the status change to MQTT.
 
 - Video device usage is detected via "lsmod" based on "uvcvideo" device. See: current_video_status()
@@ -16,32 +17,14 @@ Whenever the combined status changes, post the status change to MQTT.
   NOTE: This detects if an application like Google Meet has grabbed the microphone, but does not
         detect if the microphone is muted or not. If the host has more then one device then all are considered. 
 """
-
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(asctime)s - %(message)s')
 broker = "192.168.0.102"
 port = 1883
 alsa_input_regex = r"^alsa_input\."
+resolution_in_seconds = 2
 
-
-def connect_mqtt():
-    print(f"connecting to MQTT from host \"{platform.node()}\"...")
-
-    def on_log(client, userdata, level, buf):
-        print("mqtt: ", buf)
-
-    def on_connect(client, userdata, flags, rc):
-        if rc==0:
-            print("Successfully connected to MQTT broker")
-        else:
-            print("Failed to connect, return code %d", rc)
-    mqtt = mqtt_client.Client(platform.node())
-    # client.on_log = on_log
-    mqtt.on_connect = on_connect
-    mqtt.connect(broker, port)
-    mqtt.loop_start()
-    return mqtt
-
-
-mqtt = connect_mqtt()
+__mqtt_client = None
+__mqtt_connected = False
 
 
 def current_video_status():
@@ -64,26 +47,28 @@ def current_mic_status():
     return status
 
 
-def main():
-    # Our transmit posture is considered to be a combination of either the microphone or the video being used/grabbed
+def monitor():
+    while not __mqtt_connected:
+        time.sleep(1)
+
+    # Our status is considered to be a combination of either the microphone or the video being used/grabbed
     # by any application like Google Meet, Zoom etc. We detect the microphone and the video device state separately,
-    # we don't detect what app is using them. So for example, if you start Cheese which only uses video, and something
+    # but we don't detect what app is using them.
+    # So for example, if you start Cheese which only uses only video, something else could be using the microphone.
     # In the case of the video/webcam, we can detected if the video is live via uvcvideo such that
-    # if Google Meet is open, but the video off button is pressed, we see this. Conversely, if the
-    # video is turned on via the button, we detect the change. However, with the Microphone we
-    # only see if it is being used by the app and this is considered the Mic being live,
-    # but we don't detect if it is muted or not. Either way, if either of them are considered on
-    # then our posture is considered as on (transmitting).
-    #
-    # As you ar
-    resolution_in_seconds = 2
+    # if Google Meet is open, but the video off button is enabled, we detect it as on.
+    # Conversely, if the video is turned on via the button, we detect the change. However, with the Microphone we
+    # only see if it is being used by the app and this considers the Mic being live,
+    # but we don't detect if it is muted or not.
+    # Either way, if either of them are considered on then our posture is considered as on (transmitting).
+    logging.info(f"Starting monitoring loop. Checking every {resolution_in_seconds} sec.")
     ts = datetime.utcnow().isoformat()[:-3]+'Z'
     prior_vid_state = current_video_status()
     prior_mic_state = current_mic_status()
     prior_posture = '1' if prior_vid_state == '1' or prior_mic_state == 'RUNNING' '1' else '0'
     mqtt_topic = f"transmit_posture/{platform.node()}/status"
-    mqtt.publish(topic=mqtt_topic, payload=json.dumps({"status": prior_posture, "ts": ts}))
-    print(f"current posture: {prior_posture} ts: {ts}")
+    __mqtt_client.publish(topic=mqtt_topic, payload=json.dumps({"status": prior_posture, "ts": ts}))
+    logging.info(f"current posture: {prior_posture}")
     # when off, debounce sporadic noise which triggers on, by waiting for enough
     # resolution cycles to occur in the on position before we report it.
     on_debounce = 0
@@ -95,7 +80,7 @@ def main():
         current_posture = '0'
         if current_vid_state == '1' or current_mic_state == 'RUNNING':
             current_posture = '1'
-        # print(f"state vid: {current_vid_state} mic: {current_mic_state} posture: {current_posture} ts: {ts}")
+        logging.debug(f"state vid: {current_vid_state} mic: {current_mic_state} posture: {current_posture} ts: {ts}")
         if current_vid_state == "1" or current_mic_state == 'RUNNING':
             on_debounce += 1
         else:
@@ -106,17 +91,40 @@ def main():
             prior_vid_state = current_vid_state
             prior_mic_state = current_mic_state
             prior_posture = current_posture
-            mqtt.publish(topic=mqtt_topic, payload=json.dumps({"status": current_posture, "ts": ts}))
-            print(f"posture changed to: {current_posture} ts: {ts}")
+            __mqtt_client.publish(topic=mqtt_topic, payload=json.dumps({"status": current_posture, "ts": ts}))
+            logging.info(f"posture changed to: {current_posture}")
         elif on_debounce >= resolution_in_seconds and not notified_on:
             prior_vid_state = current_vid_state
             prior_mic_state = current_mic_state
             prior_posture = current_posture
-            mqtt.publish(topic=mqtt_topic, payload=json.dumps({"status": current_posture, "ts": ts}))
+            __mqtt_client.publish(topic=mqtt_topic, payload=json.dumps({"status": current_posture, "ts": ts}))
             notified_on = True
-            print(f"posture changed to: {current_posture} ts: {ts}")
+            logging.info(f"posture changed to: {current_posture}")
 
         time.sleep(resolution_in_seconds)
+
+
+def connect_mqtt():
+    global __mqtt_client
+    logging.info(f"connecting to MQTT broker {broker}:{port} from host \"{platform.node()}\"...")
+
+    def on_connect(client, userdata, flags, rc):
+        global __mqtt_connected
+        if rc == 0:
+            __mqtt_connected = True
+            logging.info(f"Successfully connected to MQTT broker.")
+        else:
+            logging.warning(f"Failed to connect to MQTT broker, return code {rc}")
+    __mqtt_client = mqtt_client.Client(platform.node())
+    # client.on_log = on_log
+    __mqtt_client.on_connect = on_connect
+    __mqtt_client.connect(broker, port)
+    __mqtt_client.loop_start()
+
+
+def main():
+    connect_mqtt()
+    monitor()
 
 
 if __name__ == '__main__':
---
+++ transmit-posture.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPT_PATH=/home/denis/development/transmit-posture/transmit-posture.py
+/usr/bin/python3 $SCRIPT_PATH
+exit 1
